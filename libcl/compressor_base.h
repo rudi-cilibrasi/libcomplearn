904db7d228e0c04e83f2f9d001b383ac6c9d8a80
Rudi Cilibrasi <cilibrar@morefuntechnologies.com>
2014-12-13 16:22:57 -0800

    Adding zlib back in.

--- libcl/compressor_base.h
+++ libcl/compressor_base.h
@@ -3,11 +3,38 @@
 
 #include <stdint.h>
 
+#define CL_OK 0
+#define CL_ERROR 1
+
 struct CLDatum {
   unsigned char *data;
   uint64_t length;
 };
 
-int library_func1(int a, int b);
+int library_func1(struct CLDatum cld);
+
+struct CLDatum clNewDatum(unsigned char *data, uint64_t length);
+void clFreeDatum(struct CLDatum cld);
+uint64_t clSizeDatum(struct CLDatum cld);
+unsigned char *clBytesDatum(struct CLDatum cld);
+struct CLCompressorConfig clNewConfig(void);
+
+struct CLCompressorConfig {
+  unsigned char config[1024];
+};
+
+struct CLCompressor {
+  char *(*name)(void);
+  char *(*version)(void);
+  char *(*description)(void);
+  char *(*parameters)(void);
+  int64_t  (*windowSize)(void);
+  void (*updateConfiguration)(struct CLCompressorConfig *conf, const char *k, const char *v);
+  uint64_t (*compressedSize)(struct CLDatum input, struct CLCompressorConfig *conf);
+  struct CLDatum (*compressDatum)(struct CLDatum input, struct CLCompressorConfig *conf);
+  struct CLDatum (*decompressDatum)(struct CLDatum input);
+};
+
+extern struct CLCompressor cl_zlib;
 
 #endif
--- tools/ncd.c
+++ tools/ncd.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 #include <compressor_base.h>
 
 int main(int argc, char **argv)
 {
-  printf("%d\n",library_func1(2,3));
+  struct CLDatum cld;
+  char *str = "aabaaa";
+  int len = strlen(str);
+  cld.length = len;
+  cld.data = str;
+  printf("%d\n",library_func1(cld));
   exit(0);
 }
