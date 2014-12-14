20d4df6e78651b7ed8001799d8b92ed55304c143
Rudi Cilibrasi <cilibrar@morefuntechnologies.com>
2014-12-14 15:16:56 -0800

    XZ and unitcheck related improvements.

--- libcl/cmod_xz.c
+++ libcl/cmod_xz.c
@@ -16,7 +16,9 @@ static char *icl_xz_description(void) {
 }
 
 static char *icl_xz_parameters(void) {
-  return "\"level\"\n  numeric 1-9\n  defaults 9\n  specifying how much CPU and memory to use to compress";
+  return "\"level\"\n  numeric 1-9\n  defaults 9\n  specifying how much CPU and memory to use to compress\n"
+         "\"extreme\"\n  numeric 0-1\n  defaults 0\n  enable extreme CPU modes"
+  ;
 }
 
 static char *icl_xz_version(void) {
@@ -29,6 +31,7 @@ static int64_t icl_xz_windowSize(void) {
 
 struct XZConfiguration {
   int32_t level;
+  int32_t extreme;
 };
 
 void icl_xz_updateConfiguration(struct CLCompressorConfig *conf, const char *k, const char *v)
@@ -37,16 +40,23 @@ void icl_xz_updateConfiguration(struct CLCompressorConfig *conf, const char *k, 
   if (strcmp(k, "level") == 0 && v) {
     xz_conf->level = atoi(v);
   }
+  if (strcmp(k, "extreme") == 0 && v) {
+    xz_conf->extreme = atoi(v);
+  }
 }
 
 static uint64_t icl_xz_compressedSize(struct CLDatum input, struct CLCompressorConfig *conf) {
   struct XZConfiguration *xz_conf = (struct XZConfiguration *) conf;
   int level = 9;
+  int extreme = 0;
   if (xz_conf && xz_conf->level) {
     level = xz_conf->level;
   }
+  if (xz_conf && xz_conf->extreme) {
+    extreme = xz_conf->extreme;
+  }
 	char goodcmd[32768];
-	sprintf(goodcmd, "%s -%d --compress", s_xzpath, level);
+	sprintf(goodcmd, "%s -%d %s --compress", s_xzpath, level, extreme ? "-e":"");
   uint64_t result = runCommandCount(input, goodcmd);
   return result;
 }
--- test/unitcheck/basic_complete.ts
+++ test/unitcheck/basic_complete.ts
@@ -1,10 +1,12 @@
 /* A complete test example */
 
 #include <stdio.h>
+#include <complearn.h>
 
 #test the_test
 	int nc;
-	const char msg[] = "\n\n    Hello, world!\n";
+	const char msg[] = "\n\n    Hello, CompLearn!\n";
+  clInit();
 
 	nc = printf("%s", msg);
 	ck_assert(nc == (sizeof(msg) - 1)); /* for terminating NUL. */
